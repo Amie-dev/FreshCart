cae23aa4d3fb02e54eb314e61ba964f2a3ec8db9
Amie-dev <learn.amie01@gmail.com>
2025-11-27 14:46:57 +0530

    add clientProvider or session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
-
+import ClientProvider from "@/ClientProvider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,6 +28,8 @@ export default function RootLayout({
       <body
         className= {`${geistSans.variable} ${geistMono.variable} antialiased bg-linear-to-b from-green-100 to-white w-full min-h-screen`}
       >
+        <ClientProvider>
+        
         {children}
         <ToastContainer
               position="bottom-right"
@@ -41,6 +43,7 @@ export default function RootLayout({
               pauseOnHover
               theme="dark"
             />
+            </ClientProvider>
       </body>
     </html>
   );
